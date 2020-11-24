ab5f577bb2e13bade507f1e93a471238d7d6f305
xXYvesXx <yvesh20@gmail.com>
2020-11-24 16:47:10 -0300

    criação do component Pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
     <div v-for="(poke,index) in pokemons" :key="index">
-      <h1>{{ index + 1}} {{ poke.name}}</h1>
+      <Pokemon :name="poke.name" url="poke.url" :num="index+1" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Pokemon from './components/Pokemon'
 export default {
   name: 'App',
   data(){
@@ -19,6 +20,9 @@ export default {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
       this.pokemons = res.data.results
     })
+  },
+  components: {
+    Pokemon
   }
 }
 </script>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ num }}{{name}}</h1>
+        <small>{{url}}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    }
+}
+
+</script>
+
+<style lang="stylus">
+
+</style>
